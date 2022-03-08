c9a0eea07f90dc738411b12501032cd60efa91e9
magiruuvelvet <magiruuvelvet@naver.com>
2022-03-08 13:40:09 +0100

    uuidgen: new plugin

---
+++ uuidgen/__init__.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+
+"""UUID Generator"""
+
+from albert import *;
+import uuid;
+
+import os;
+from pydoc import importfile;
+module_path = os.path.realpath(os.path.dirname(__file__));
+
+# dep = importfile(module_path + "/dep.py");
+
+__title__ = "UUID Generator";
+__version__ = "0.0.1";
+__triggers__ = "uuidgen";
+__authors__ = "マギルゥーベルベット";
+
+iconPath = iconLookup("dialog-password");
+
+def handleQuery(albertQuery: Query) -> list[Item]:
+    # check if the query has a trigger
+    if not albertQuery.isTriggered:
+        return [];
+
+    generated_uuid = str(uuid.uuid4());
+
+    return [
+        Item(
+            id=__title__,
+            icon=iconPath,
+            text=generated_uuid,
+            completion=generated_uuid,
+            urgency=ItemBase.Normal,
+            actions=[
+                ClipAction(text="ClipAction", clipboardText=generated_uuid),
+            ],
+        )
+    ];
